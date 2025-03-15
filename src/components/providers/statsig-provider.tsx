c4edbd8f97378592501ec7ed13bf62026222a172
Enrique <sotomaque@gmail.com>
2025-03-14 22:46:46 -0700

    chore: add statsig fallback

--- src/components/providers/statsig-provider.tsx
+++ src/components/providers/statsig-provider.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import React from 'react';
+import type { ReactNode } from 'react';
 import { LogLevel, StatsigProvider } from '@statsig/react-bindings';
 import { StatsigAutoCapturePlugin } from '@statsig/web-analytics';
 import { env } from '@/env';
+import { LoadingSpinner } from '../ui/loading-spinner';
 
-export default function MyStatsig({ children }: { children: React.ReactNode }) {
+export default function MyStatsig({ children }: { children: ReactNode }) {
   return (
     <StatsigProvider
       sdkKey={env.NEXT_PUBLIC_STATSIG_CLIENT_KEY!}
@@ -14,6 +15,7 @@ export default function MyStatsig({ children }: { children: React.ReactNode }) {
         logLevel: LogLevel.Debug,
         plugins: [new StatsigAutoCapturePlugin()],
       }}
+      loadingComponent={<LoadingSpinner />}
     >
       {children}
     </StatsigProvider>
